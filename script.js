96d1afb54873beb6d23ba7ebf4eafb55b04deac0
Nikhil Singh <Singh.nikhil999@gmail.com>
2016-12-21 19:13:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,71 @@
+function getAverageWordsPerSentence(text) {
+  
+  var numSentences = text.match(/[.!?]+/g) ? text.match(/[.!?]+/g).length : 1;
+  var wordCount = tokenizeText(text).length;
+  return (wordCount / numSentences).toFixed(2);
+}
+
+function getAverageWordLength(tokens) {
+
+  var totalLength = tokens.join("").length;
+  return (totalLength / tokens.length).toFixed(2);
+}
+
+function countDistinctWords(tokens) {
+  
+  var distinctWords = [];
+  for (var i=0; i<tokens.length; i++) {
+    if (distinctWords.indexOf(tokens[i]) === -1) {
+      distinctWords.push(tokens[i]);
+    }
+  }
+  return distinctWords.length;
+}
+
+function tokenizeText(text) {
+  return text.toLowerCase().match(/\b[^\s]+\b/g).sort();
+}
+
+
+function removeReturns(text) {
+  return text.replace(/\r?\n|\r/g, "");
+}
+
+
+
+// generate and display analytics on text
+
+function reportOnText(text) {
+  // tokenize our text then compute our data points
+
+  var tokens = tokenizeText(text);
+  var numDistinctWords = countDistinctWords(tokens);
+  var numTotalWords = tokens.length;
+  var averageWordLength = getAverageWordLength(tokens);
+  var averageWordsPerSentence = getAverageWordsPerSentence(text);
+
+  // take our data and display it in the dom
+  var textReport = $('.js-text-report');
+  textReport.find('.js-word-count').text(numTotalWords);
+  textReport.find('.js-unique-word-count').text(numDistinctWords);
+  textReport.find('.js-average-word-length').text(
+    averageWordLength + " characters");
+  textReport.find('.js-average-sentence-length').text(
+    averageWordsPerSentence + " words");
+  textReport.removeClass('hidden');
+}
+
+
+function watchFormSubmission() {
+  $('.js-text-form').submit(function(event) {
+    event.preventDefault();
+    // get the text the user submitted
+    var userText = $(this).find('#user-text').val();
+    reportOnText(removeReturns(userText));
+  });
+}
+
+// equivalent to `$(document).ready(function() {...})`
+$(function() {
+  watchFormSubmission();
+});
